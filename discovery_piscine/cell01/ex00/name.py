20218d8d66b9cb8b158397b23d493e675896a67d
AruchaK <aruchakhem@gmail.com>
2026-09-21 13:18:17 +0700

    feat: cell01 exercises

---
+++ discovery_piscine/cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Arucha"
+last_name = "Khematharonon"
+print(first_name, last_name)
---
+++ discovery_piscine/cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Arucha"
+last_name = "Khematharonon"
+whole_name = first_name + " " + last_name
+print(whole_name)
