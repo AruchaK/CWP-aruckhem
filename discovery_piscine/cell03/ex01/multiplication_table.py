4fb1e356ded52066116b3baf29082fa0701d6af8
AruchaK <aruchakhem@gmail.com>
2026-09-22 01:26:44 +0700

    feat: cell03 exercises

---
+++ discovery_piscine/cell03/ex01/multiplication_table.py
@@ -0,0 +1,2 @@
+x = (int(input("Enter a number:\n")))
+print("\n".join(f"{i} x {x} = {x * i}" for i in range(0, 10)))
---
+++ discovery_piscine/cell03/ex02/i_got_that.py
@@ -0,0 +1,5 @@
+input("What you gotta say? : ")
+while True:
+    x = input("I got that! Anything else? : ")
+    if x == "STOP":
+        break
